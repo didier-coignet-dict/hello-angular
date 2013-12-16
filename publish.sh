a51f37dd7f09136b7e666d85cecff6d2e10f519b
yann <contact@yannlombard.com>
2013-12-17 00:45:11 +0100

    gitignore update Publish.sh update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 # publish dist to gh-pages
 # http://yeoman.io/deployment.html
+git add dist && git commit -m "Auto publish on gh-pages"
+git subtree push --prefix dist origin gh-pages
 
 #cd C:\wamp\www\hello-angular
 #git add *
